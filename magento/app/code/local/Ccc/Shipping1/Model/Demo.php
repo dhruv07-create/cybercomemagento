ff42cfec40b265a2ef70b32fe317f37cb4ccbfd9
Dhruv <mrdhruvbhuva07@gmail.com>
2021-05-20 21:49:55 -0700

    first

---
+++ magento/app/code/local/Ccc/Shipping1/Model/Demo.php
@@ -0,0 +1,105 @@
+<?php
+class Ccc_Shipping1_Model_Demo extends Mage_Shipping_Model_Carrier_Abstract implements Mage_Shipping_Model_Carrier_Interface
+{
+
+   protected $_code = 'envato_customshippingmethod';
+   protected $_isFixed = true;
+   protected $_isInitializeNeeded      = true;
+   protected $_canUseInternal          = true;
+   protected $_canUseForMultishipping  = false;
+ 
+ /* public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+  {
+    $result = Mage::getModel('shipping/rate_result');
+    $result->append($this->_getDefaultRate());
+ 
+    return $result;
+  }*/
+  public function collectRates(Mage_Shipping_Model_Rate_Request $request)
+    {
+        if (!$this->getConfigFlag('active')) {
+            return false;
+        }
+
+        $freeBoxes = 0;
+        if ($request->getAllItems()) {
+            foreach ($request->getAllItems() as $item) {
+
+                if ($item->getProduct()->isVirtual() || $item->getParentItem()) {
+                    continue;
+                }
+
+                if ($item->getHasChildren() && $item->isShipSeparately()) {
+                    foreach ($item->getChildren() as $child) {
+                        if ($child->getFreeShipping() && !$child->getProduct()->isVirtual()) {
+                            $freeBoxes += $item->getQty() * $child->getQty();
+                        }
+                    }
+                } elseif ($item->getFreeShipping()) {
+                    $freeBoxes += $item->getQty();
+                }
+            }
+        }
+        $this->setFreeBoxes($freeBoxes);
+
+        $result = Mage::getModel('shipping/rate_result');
+        if ($this->getConfigData('type') == 'O') { // per order
+            $shippingPrice = $this->getConfigData('price');
+        } elseif ($this->getConfigData('type') == 'I') { // per item
+            $shippingPrice = ($request->getPackageQty() * $this->getConfigData('price')) - ($this->getFreeBoxes() * $this->getConfigData('price'));
+        } else {
+            $shippingPrice = false;
+        }
+
+        $shippingPrice = $this->getFinalPriceWithHandlingFee($shippingPrice);
+
+        if ($shippingPrice !== false) {
+            $method = Mage::getModel('shipping/rate_result_method');
+
+            $method->setCarrier('envato_customshippingmethod');
+            $method->setCarrierTitle($this->getConfigData('title'));
+
+            $method->setMethod('envato_customshippingmethod');
+            $method->setMethodTitle($this->getConfigData('name'));
+
+            if ($request->getFreeShipping() === true || $request->getPackageQty() == $this->getFreeBoxes()) {
+                $shippingPrice = '0.00';
+            }
+
+
+            $method->setPrice($shippingPrice);
+            $method->setCost($shippingPrice);
+
+            $result->append($method);
+        }
+
+        return $result;
+    }
+ 
+ /* public function getAllowedMethods()
+  {
+    return array(
+      'envato_customshippingmethod' => $this->getConfigData('name'),
+    );
+  }*/
+
+    public function getAllowedMethods()
+    {
+        return array('envato_customshippingmethod'=>$this->getConfigData('name'));
+    }
+ 
+  protected function _getDefaultRate()
+  {
+    $rate = Mage::getModel('shipping/rate_result_method');
+     
+    $rate->setCarrier($this->_code);
+    $rate->setCarrierTitle($this->getConfigData('title'));
+    $rate->setMethod($this->_code);
+    $rate->setMethodTitle($this->getConfigData('name'));
+    $rate->setPrice($this->getConfigData('price'));
+    $rate->setCost(0);
+     
+    return $rate;
+  }
+
+}
